22c36e2dc57f029e041efd0b09d78c730148cbf9
THEDUKE <alexis.leduc.j@gmail.com>
2023-09-26 14:22:30 +0100

    dev: ajout du type narrowing

---
+++ front/src/constant.ts
@@ -0,0 +1,6 @@
+const cx0 = 0;
+const cy0 = 0;
+const r0 = 55;
+const svgns = "http://www.w3.org/2000/svg";
+const samples = 100;
+const multiplicationFactor = 7;
--- front/src/main.ts
+++ front/src/main.ts
@@ -2,21 +2,27 @@ console.log("CCOUCOURR");
 
 //add small circle
 const sampleContainer = document.querySelector("g.samples");
+if (sampleContainer === null) {
+  throw new Error("OUPSSS");
+}
 for (let i = 0; i < samples; i++) {
   const angle = (i * 2 * Math.PI) / samples + Math.PI / 2;
   const cx = cx0 + r0 * Math.cos(angle);
   const cy = cy0 + r0 * Math.sin(angle);
   const circle = document.createElementNS(svgns, "circle");
 
-  circle.setAttributeNS(null, "cx", cx);
-  circle.setAttributeNS(null, "cy", cy);
-  circle.setAttributeNS(null, "r", 1);
+  circle.setAttributeNS(null, "cx", cx + "");
+  circle.setAttributeNS(null, "cy", cy.toString());
+  circle.setAttributeNS(null, "r", "1");
 
   sampleContainer.appendChild(circle);
 }
 
 //draw lines
 const lineContainer = document.querySelector("g.lines");
+if (lineContainer === null) {
+  throw new Error("OUPSSS");
+}
 for (let i = 0; i < samples; i++) {
   const angle1 = (i * 2 * Math.PI) / samples - Math.PI / 2;
   const x1 = cx0 + r0 * Math.cos(angle1);
@@ -28,10 +34,10 @@ for (let i = 0; i < samples; i++) {
   const y2 = cy0 + r0 * Math.sin(angle2);
   const lines = document.createElementNS(svgns, "line");
 
-  lines.setAttributeNS(null, "x1", x1);
-  lines.setAttributeNS(null, "y1", y1);
-  lines.setAttributeNS(null, "x2", x2);
-  lines.setAttributeNS(null, "y2", y2);
+  lines.setAttributeNS(null, "x1", x1.toString());
+  lines.setAttributeNS(null, "y1", y1.toString());
+  lines.setAttributeNS(null, "x2", x2.toString());
+  lines.setAttributeNS(null, "y2", y2.toString());
 
   lineContainer.appendChild(lines);
 }
