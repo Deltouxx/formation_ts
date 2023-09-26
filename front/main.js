6eef390709bf2b59da57664e36e7cbeb02646fd8
THEDUKE <alexis.leduc.j@gmail.com>
2023-09-26 11:19:39 +0100

    refactor: added constant

--- front/main.js
+++ front/main.js
@@ -1,14 +1,7 @@
 console.log("CCOUCOURR");
 
-const cx0 = 0;
-const cy0 = 0;
-const r0 = 55;
-const svgns = "http://www.w3.org/2000/svg";
-const container = document.querySelector("g.samples");
-const samples = 300;
-const multiplicationFactor = 3;
-
 //add small circle
+const sampleContainer = document.querySelector("g.samples");
 for (let i = 0; i < samples; i++) {
   const angle = (i * 2 * Math.PI) / samples + Math.PI / 2;
   const cx = cx0 + r0 * Math.cos(angle);
@@ -19,7 +12,7 @@ for (let i = 0; i < samples; i++) {
   circle.setAttributeNS(null, "cy", cy);
   circle.setAttributeNS(null, "r", 1);
 
-  container.appendChild(circle);
+  sampleContainer.appendChild(circle);
 }
 
 //draw lines
