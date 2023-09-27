9547214a4acd0cac444d163cea162c689e76e449
THEDUKE <alexis.leduc.j@gmail.com>
2023-09-27 15:38:00 +0100

    dev: add serv

--- back/api.js
+++ back/api.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const app = express.Router();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.get("/random-config", (req, res) => {
-  res.json({ samples: 50, multiplicationFactor: 50 });
+  res.json({ samples: 500, multiplicationFactor: 14.5 });
 });
 module.exports = app;
